b324bfe4be78c6625e0c08d80cf8e8af6c42f52d
Vuks69 <51289041+Vuks69@users.noreply.github.com>
2024-03-10 16:31:20 +0100

    Add template

---
+++ 2023/template.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+with open("inputs/num.txt") as f:
+    puzzle_input = f.read()
+
+
+def part1(puzzle_input):
+    pass
+
+
+def part2(puzzle_input):
+    pass
+
+
+print(part1(puzzle_input))
+print(part2(puzzle_input))

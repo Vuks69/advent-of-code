54903dee2e6a43e570fc1adf99ff1bdcddfc5e28
Vuks69 <51289041+Vuks69@users.noreply.github.com>
2024-03-24 14:23:49 +0100

    Finished day 5

--- 2023/day05.py
+++ 2023/day05.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python
-import re
 from itertools import chain
 from multiprocessing import Pool
 
 with open("inputs/05.txt") as f:
     puzzle_input = f.read().splitlines()
 
-variables = [
+stages = [
     "seed",
     "soil",
     "fertilizer",
@@ -16,9 +15,9 @@ variables = [
     "humidity",
     "location",
 ]
-steps = [f"{variables[i]}-to-{variables[i+1]}" for i in range(len(variables) - 1)]
+steps = [f"{stages[i]}-to-{stages[i+1]}" for i in range(len(stages) - 1)]
 
-almanac = {}
+almanac: {str: [dict]} = {}
 for step_name in steps:
     almanac[step_name] = []
     start = puzzle_input.index(f"{step_name} map:")
@@ -27,28 +26,24 @@ for step_name in steps:
     except ValueError:
         stop = None
     for line in puzzle_input[start + 1 : stop]:
-        range_dest, range_src, range_len = line.split(" ")
+        dest_start, src_start, range_len = line.split(" ")
         almanac[step_name].append(
             {
-                "dst": int(range_dest),
-                "src": int(range_src),
-                "len": int(range_len),
+                "dst": range(int(dest_start), int(dest_start) + int(range_len)),
+                "src": range(int(src_start), int(src_start) + int(range_len)),
             }
         )
-
-
-def get_destination(step_name: str, source: int) -> int:
-    for entry in almanac[step_name]:
-        if entry["src"] <= source <= entry["src"] + entry["len"]:
-            offset = source - entry["src"]
-            return entry["dst"] + offset
-    return source
+    almanac[step_name].sort(key=lambda e: e["src"].start)
 
 
 def get_location(seed: int) -> int:
     next_source = seed
     for step in steps:
-        next_source = get_destination(step, next_source)
+        for entry in almanac[step]:
+            if entry["src"].start <= next_source < entry["src"].stop:
+                offset = entry["src"].index(next_source)
+                next_source = entry["dst"][offset]
+                break
     return next_source
 
 
@@ -59,20 +54,37 @@ def part1(puzzle_input):
 
 def part2(puzzle_input):
     seed_data = list(map(int, puzzle_input[0].removeprefix("seeds: ").split(" ")))
-
     # seed_ranges = [range(123456, 234567), range(345678, 456789), ...]
-    seed_ranges = map(
-        lambda sr: range(sr[0], sum(sr)), zip(seed_data[0::2], seed_data[1::2])
+    seed_ranges = sorted(
+        map(lambda sr: range(sr[0], sum(sr)), zip(seed_data[0::2], seed_data[1::2])),
+        key=lambda r: r.start,
     )
+    intervals = [(r, 0) for r in seed_ranges]
+    min_loc = float("inf")
+    while intervals:
+        r, stage = intervals.pop()
+        if stage == 7:
+            min_loc = min(r.start, min_loc)
+            continue
 
-    # seeds ~= [1234567, 1234568, ...] - not evaluated to memory, but seeds.__next__() gives next number,
-    # which is what map and Pool.map care about (Iterable objects).
-    seeds = chain.from_iterable(seed_ranges)
+        for entry in almanac[steps[stage]]:
+            src: range = entry["src"]
+            dst: range = entry["dst"]
+            offset = dst.start - src.start
+            if r.stop <= src.start or src.stop <= r.start:
+                continue
+            if r.start < src.start:
+                intervals.append((range(r.start, src.start), stage))
+                r = range(src.start, r.stop)
+            if src.stop < r.stop:
+                intervals.append((range(src.stop, r.stop), stage))
+                r = range(r.start, src.stop)
+            intervals.append((range(r.start + offset, r.stop + offset), stage + 1))
+            break
+        else:
+            intervals.append((r, stage + 1))
 
-    with Pool() as pool:
-        results = min(pool.imap_unordered(get_location, seeds, 10**6))
-    # results = map(get_location, seeds) # Too slow.
-    return results
+    return min_loc
 
 
 print(part1(puzzle_input))

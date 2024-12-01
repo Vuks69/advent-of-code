854e1be3c045b4e5b409b6690130bcd5f4c85d91
Vuks69 <51289041+Vuks69@users.noreply.github.com>
2024-12-01 11:48:19 +0100

    mixxing export

--- templates/generate_next_day.sh
+++ templates/generate_next_day.sh
@@ -4,8 +4,8 @@ SCRIPTPATH="$(
     cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit 1
     pwd -P
 )"
-day_number="${1:?Need day number}"
+export day_number="${1:?Need day number}"
 year="${2:-2024}"
 
-touch "${SCRIPTPATH}/../${year}inputs/${day_number}.txt"
+touch "${SCRIPTPATH}/../${year}/inputs/${day_number}.txt"
 envsubst <"${SCRIPTPATH}/template.py.tpl" >"${SCRIPTPATH}/../${year}/day${day_number}.py"
